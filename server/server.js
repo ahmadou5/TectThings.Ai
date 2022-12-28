341dc81bbfae155b7366e1980e3d66368fe42c08
ahmadou5 <78315869+ahmadou5@users.noreply.github.com>
2022-12-28 23:53:47 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -45,4 +45,4 @@ app.post('/', async (req, res) => {
 })
 
 
-app.listen(5000,() => console.log('server is running on port http://localhost:5000'));
+app.listen(https://tect-things-ai.vercel.app,() => console.log('server is running on port http://localhost:5000'));
